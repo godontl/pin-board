3359d13f8c20e622bb59e9c0f3d90f3dbaa0ffd2
Truelove Godonou <lovegodonou@gmail.com>
2022-06-07 16:30:43 +0200

    posso modificare i commenti

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,17 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :find_pin
+  before_action :find_comment, except: [:create]
 
-  #  def index
-  #    @comment = Comment.all
-  #  end
+  def index
+      @comment = Comment.all
+    end
 
-  # def new
-  #   @comment = Comment.build
-  # end
+    def new
+      user = session[:user_id]
+      @comment = Comment.new(post_id: params[:post_id])
+      @post = Post.find(params[:post_id])
+    end
 
   def create
     @comment = Comment.create(comment_params)
@@ -21,8 +24,23 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @comment.user == current_user
+      @comment.body = params["comment"]["body"]
+      if @comment.save!
+        redirect_to '/pins', notice: "Il commento è stato modificato correttamente!"
+      else
+        render 'edit'
+      end
+    else
+      redirect_to '/pins', notice: "Il commento non può essere modificato perchè non sei l'autore."
+    end
+  end
+
   def destroy
-    @comment = Comment.find(params[:id])
     if @comment.user == current_user || @pin.user == current_user
       @comment.destroy
       redirect_to pin_path(@pin), notice: "Il Commento è stato cancellato correttamente!"
@@ -37,5 +55,8 @@ class CommentsController < ApplicationController
     end
     def find_pin
       @pin = Pin.find(params[:pin_id])
+    end
+    def find_comment
+      @comment = Comment.find(params[:id])
     end
 end
